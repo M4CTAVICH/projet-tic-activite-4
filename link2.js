1761fb165cc1f562503720afd78ba43f333b807b
M4CT4V1CH <chakibmessaoud.ouaret@etu.usthb.dz>
2023-05-16 05:14:18 +0100

    first commit

---
+++ link2.js
@@ -0,0 +1,19 @@
+alert('please our work is not compatible with some navigators be sure to be using one of the following browsers : chrome , opera ,firefox or microsoft edge. thank you :)')
+var typed = new Typed(".auto", { 
+    strings: ["ACTIVITÉ 4"] ,
+    typeSpeed: 150,
+    backSpeed: 0,
+    loop: true
+    
+    
+    
+})
+var typed = new Typed(".auto2", { 
+    strings: ["FIN"] ,
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+    
+    
+    
+})
